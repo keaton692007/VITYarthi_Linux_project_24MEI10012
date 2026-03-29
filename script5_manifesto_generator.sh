319317ba542c5df88ace936c7ea845f5b53d1b0f
keaton692007 <keatonweber69@gmail.com>
2026-03-29 14:27:21 +0530

    Update script5_manifesto_generator.sh

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script 5: Open Source Manifesto Generator
-# Author: Puli Sai Chaithanya Kumar
+# Author: Kurupati Ranganayakulu
 # Course: Open Source Software
 
 echo "Answer the following questions to generate your Open Source Manifesto."
