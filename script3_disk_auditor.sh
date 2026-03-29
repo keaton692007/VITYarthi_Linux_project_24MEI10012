8300ffee17503917ff5b4b74009213d59477a78c
keaton692007 <keatonweber69@gmail.com>
2026-03-29 14:26:55 +0530

    Update script3_disk_auditor.sh

--- script3_disk_auditor.sh
+++ script3_disk_auditor.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # Script 3: Disk and Permission Auditor
-# Author: Puli Sai Chaithanya Kumar
+# Author: Kurupati Ranganayakulu
 # Course: Open Source Software
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
